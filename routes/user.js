bc04891190c12a39c210e5da31ea008e11080df1
Kunal Vats <1kunalvats9@gmail.com>
2025-04-14 22:48:01 +0530

    done

--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,16 @@
 import express from "express";
-import mongoose from "mongoose";
 import User from "../models/userModel.js";
 
 const router = express.Router();
 
-
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-  console.log("MongoDB connected");
-}).catch(err => console.log("DB Error:", err));
-
-
 router.post('/get-user', async (req,res)=>{
     try{
-        const {email} = await req.json();
-        const user = await User.findOne({email})
+        const { email } = req.body; 
+        const user = await User.findOne({ email });
         if(!user){
             return res.status(400).json({ error: 'user not found' });
         }
-        return res.status(200).json({success:true,data:user})
+        return res.status(200).json({ success: true, data: user });
 
     }catch(err){
         console.error("❌ Error in getting the user:", err);
@@ -25,4 +18,4 @@ router.post('/get-user', async (req,res)=>{
     }
 })
 
-export default router
+export default router;
