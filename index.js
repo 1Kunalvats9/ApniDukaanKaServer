8473347c3b94a8187342fec8226176e71b74c2cb
Kunal Vats <1kunalvats9@gmail.com>
2025-04-14 23:47:02 +0530

    done

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ import inventoryRoutes from "./routes/inventory.js";
 import smsRoutes from "./routes/sms.js";
 import checkoutRoutes from "./routes/checkout.js";
 import userRoutes from "./routes/user.js"
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 
 dotenv.config();
 
@@ -22,8 +26,14 @@ app.use('/api/inventory', inventoryRoutes);
 app.use("/api/sms", smsRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use('/api/user',userRoutes)
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Serve static files from the React build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Handle all other routes by serving index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
